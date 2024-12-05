f890b96ecaf1cb8c90849f70d1aeb80e81ba2275
MetAngel <152192071+MetAngel@users.noreply.github.com>
2024-12-05 15:20:54 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // JavaScript Document
 console.log("hi");
+
+// Hulp met modal voor hamburger menu https://www.w3schools.com/howto/howto_css_modals.asp
+
+// const modal = document.querySelector(#hamburger-menu);
+// const modalbtn = document.querySelector(#menu-btn);
+
+// modalbtn.onclick = function() {
+//     modal.style.display ="block";
+// }
+
+
+
